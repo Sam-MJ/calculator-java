8b16dd4853b6d4421285f82bc8e8708cec4b9411
Sam-MJ <tf2redspark@gmail.com>
2025-09-10 13:57:23 +0100

    extracted input into its own class so that multiple input methods can be created in the future such as parsing from args input, added try catch so it doesn't crash on invalid input, now you aren't forced to alternate between numbers and operators

--- src/main/java/com/mycompany/calculator/Calculator.java
+++ src/main/java/com/mycompany/calculator/Calculator.java
@@ -1,38 +1,11 @@
 package com.mycompany.calculator;
 
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.regex.*;
 
 public class Calculator {
 
-    private Pattern operatorPattern = Pattern.compile("[*\\-+/=\\(\\)\\^]");
-
-    private ArrayList<String> getInput(){
-        ArrayList<String> inputList = new ArrayList<String>();
-
-        Integer inputNumber;
-        String operation = "";
-
-        Scanner scanner = new Scanner(System.in);
-
-        while (!operation.contains("=")) {
-
-            System.out.println("Input a number");
-            inputNumber = scanner.nextInt();
-            inputList.add(inputNumber.toString());
-
-            System.out.println("Input your operation");
-            operation = scanner.next(operatorPattern).substring(0,1);
-            inputList.add(operation);
-        }
-
-        scanner.close();
-        return inputList;
-    }
-
     public ArrayList<String> run(){
-        ArrayList<String> inputs = getInput();
+        ArrayList<String> inputs = Input.getInput();
         ArrayList<String> reversePolishConvertedList = RPNConverter.shuntingYard(inputs);
         return reversePolishConvertedList;
     }
